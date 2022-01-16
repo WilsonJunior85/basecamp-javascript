8e124b2051aeb26f2082b3cbe146b42bbe0034c5
Wilson Júnior <69021635+WilsonJunior85@users.noreply.github.com>
2022-01-16 20:52:05 -0300

    Add files via upload

---
+++ modulos/main.mjs
@@ -0,0 +1,5 @@
+import { mostraCidade, mostraIdade, mostraHobby } from "./funcoes.mjs";
+
+console.log(mostraIdade('Wilson', 36));
+console.log(mostraCidade('Wilson', 'Rio de Janeiro'));
+console.log(mostraHobby('Wilson', 'Futebol'));
